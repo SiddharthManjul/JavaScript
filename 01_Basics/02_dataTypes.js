0e3a585c8c285bed4c5b86927122bff1d41e02d7
SiddharthManjul <manjulsiddharth09@gmail.com>
2024-01-08 00:08:13 +0530

    Revised Variables & DataTypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+// code readability should be high
+
+console.log("Brooklyn")
+
+
+let name = "Brooklyn"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint => For using very Big Numbers Ex: Stock Market analysis
+// string => Prefer to use ""
+// boolean => true/false
+// null => standalone value & DataType
+// undefined => when variable is initialized but not defined
+// symbol => unique Ex: To show the uniqueness of every component in ReactJS
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
